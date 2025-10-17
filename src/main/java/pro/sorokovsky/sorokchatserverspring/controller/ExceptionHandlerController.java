1a2fa0b7ac2452b1cb30d8d9d8fc6d937c8d35c8
Sorokovsky <Sorokovskys@ukr.net>
2025-10-17 18:53:57 +0300

    translating validation errors(ukrainian)

--- src/main/java/pro/sorokovsky/sorokchatserverspring/controller/ExceptionHandlerController.java
+++ src/main/java/pro/sorokovsky/sorokchatserverspring/controller/ExceptionHandlerController.java
@@ -3,12 +3,16 @@ package pro.sorokovsky.sorokchatserverspring.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import pro.sorokovsky.sorokchatserverspring.contract.ErrorModel;
 import pro.sorokovsky.sorokchatserverspring.exception.base.HttpException;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 @ControllerAdvice
 @RequiredArgsConstructor
@@ -26,5 +30,16 @@ public class ExceptionHandlerController {
         return ResponseEntity
                 .status(exception.getStatusCode())
                 .body(new ErrorModel(exception.getStatusCode().value(), message));
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<Map<String, String>> handleBindException(BindException exception) {
+        final var errors = new HashMap<String, String>();
+        exception.getBindingResult().getAllErrors().forEach(error -> {
+            final var key = ((FieldError) error).getField();
+            final var value = error.getDefaultMessage();
+            errors.put(key, value);
+        });
+        return ResponseEntity.badRequest().body(errors);
     }
 }
